58d4f80b417227497b9871123f03f8186cdc6b80
Pallab Rath <pallab_rath@hotmail.com>
2020-02-12 18:03:12 +0530

    Add files via upload

---
+++ vlog/gcp/php-mysql-appEngine-clouddb /src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PHP running in GCP App Engine</h1>
+
+<?php
+echo "Hello World!";
+?>
+<br/><a href="mysqli-example.php">mysqli-example</a><br/>
+<a href="pdo-example.php">pdo-example</a>br/>
+<a href="pdo-dsn-example.php">pdo-dsn-example</a>br/>
+</body>
+</html>
